0c571a1cbc5e8872bb9ebe96bd6367f85f3e4765
Paulo Weslley <vorfast@gmail.com>
2022-09-11 17:17:17 -0300

    ajustes gerais das dependencias dos modulos

--- app_cliente_registro_protocolo_mob/lib/main.dart
+++ app_cliente_registro_protocolo_mob/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:app_cliente_registro_protocolo_mob/firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:dependencies_module/dependencies_module.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
