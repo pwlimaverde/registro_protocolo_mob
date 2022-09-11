3a1403d78c16fd0ff36a67093f92e68e611ae987
Paulo Weslley <vorfast@gmail.com>
2022-09-11 17:04:55 -0300

    dependencias basicas ok

--- modulos/dependencies_module/lib/dependencies_module.dart
+++ modulos/dependencies_module/lib/dependencies_module.dart
@@ -1,7 +1,17 @@
 library dependencies_module;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+///Pacakages Externos
+export 'package:return_success_or_error/return_success_or_error.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_storage/firebase_storage.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:firebase_remote_config/firebase_remote_config.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+///Modulos
+export 'package:design_system_module/design_system_module.dart';
+export 'package:core_module/core_module.dart';
+export 'package:splash_module/splash_module.dart';
+export 'package:login_module/login_module.dart';
+export 'package:home_module/home_module.dart';
--- modulos/home_module/lib/home_module.dart
+++ modulos/home_module/lib/home_module.dart
@@ -1,7 +1,1 @@
 library home_module;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
